da3316d8e6b5ab439ab33550d6b65e06e0edf8e0
Sheikh Mahmudul <mahmuduls451@gmail.com>
2024-12-13 22:57:58 +0600

    java Basic

---
+++ src/Mahmud/DataTypes.java
@@ -0,0 +1,25 @@
+package Mahmud;
+
+public class DataTypes {
+    public static void main(String[] args) {
+//        int number  = 5;
+//        float floatNumber = 5.5f;
+//        double doubleNumber = 5565;
+//        char charNumber = 'A';
+//        boolean  booleanNumber = true;
+
+//        System.out.println("Char Value: "+charNumber);
+//        System.out.println("Boolean: "+booleanNumber);
+//        System.out.println("Double Number: "+doubleNumber);
+//        System.out.println("Float Number: "+floatNumber);
+//
+//        System.out.println("Int Number: "+number+"%nChar Value: "+charNumber+"%nBoolean: "+booleanNumber+"%nDouble Number: "+doubleNumber+"%nFloat Number: "+floatNumber);
+//        System.out.printf("Hello, World!%nWelcome to Java!");
+
+        String name = "Java";
+        System.out.println("Hello, " + name);
+        int[] numbers = {10, 20, 30};
+        System.out.println("First number: " + numbers[0]);
+    }
+}
+
---
+++ src/Mahmud/Variables.java
@@ -0,0 +1,7 @@
+package Mahmud;
+
+public class Variables {
+    public static void main(String[] args) {
+
+    }
+}
